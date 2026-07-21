577e6518b39ed2704084ea4b56d3e857d1afdedc
stlafit02-dev <stlaf.it02@gmail.com>
2026-07-21 14:13:01 +0800

    Fix unused Navigate import for Vercel build

--- stlaf.frontend/src/App.tsx
+++ stlaf.frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import ProtectedRoute from "./routes/ProtectedRoute";
 import AdminLayout from "./layouts/AdminLayout";
